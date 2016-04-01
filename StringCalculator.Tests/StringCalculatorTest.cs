e6df70f35e782d5ff7c2eef6ecc36030d7d1e4bd
Rob Bell <me@rob-bell.net>
2016-04-01 12:22:17 +0100

    Add single number implementation

--- StringCalculator.Tests/StringCalculatorTest.cs
+++ StringCalculator.Tests/StringCalculatorTest.cs
@@ -1,6 +1,39 @@
-﻿namespace StringCalculator.Tests
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests
 {
+    [TestFixture]
     public class StringCalculatorTest
     {
+        [Test]
+        public void EmptyStringReturnsZero()
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Add(String.Empty);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NumberStringReturnsIntegerEquivalent()
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Add("2");
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            if(String.IsNullOrEmpty(number)) return 0;
+
+            return int.Parse(number);
+        }
     }
 }
